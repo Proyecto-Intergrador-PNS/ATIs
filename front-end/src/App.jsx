3a117167035504b5d0d38612de8f4fff75a9de94
TenshiHimSelf <jrcontreras446@gmail.com>
2025-07-25 01:17:46 -0600

    server side is done, working con font-end login logic, code 59.00m

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
+import Root from './components/Root'
 
 function App() {
 
   return (
-    <div className='text-3xl font-bold underline'>
-      React App
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/admin/dashboard" element={<h1>Admin Dashboard</h1>} />
+        <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,11 +1,12 @@
 import bcrypt from 'bcrypt';
-import User from './models/User.js';
+import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
     if (!user) {
       return res.status(401).json({success:false, message: "User not found" });
     }
@@ -17,11 +18,10 @@ const login = async (req, res) => {
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '90d' });
 
-    return res.status(200).json({success:true, message: "Login successful", token, user: { id: user._id, name: user.name, email: user.email, role: user.role } });
+    return res.status(200).json({success:true, message: "Login successful", token, user: { id: user._id, name: user.name, email: user.email, role: user.role }});
 
 
   } catch (error) {
-    console.error("Login error:", error.message);
     return res.status(500).json({success:false, message: "Internal server error" });
   }
 }
