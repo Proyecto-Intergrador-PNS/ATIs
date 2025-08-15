7ea5df5c011d8ec15e0fd0b8afae6228911befae
TenshiHimSelf <jrcontreras446@gmail.com>
2025-08-14 22:40:51 -0600

    Category module added

--- front-end/src/components/Categories.jsx
+++ front-end/src/components/Categories.jsx
@@ -1,16 +1,17 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { data } from 'react-router'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { data } from 'react-router';
 
 const Categories = () => {
-  const [categoryName, setCategoryName] = useState("")
-  const [categoryDescription, setCategoryDescription] = useState("")
-  const [categories, setCategories] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [editCategory, setEditCategory] = useState(null);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
+
+  const fetchCategories = async () => {
       setLoading(true);
       try {
         const response = await axios.get("http://localhost:3000/api/category", {
@@ -24,15 +25,33 @@ const Categories = () => {
       } catch (error) {
         console.error("Error fetching categories:", error);
         setLoading(false);
-      }
+      };
     };
 
+
+  useEffect(() => {
     fetchCategories();
   }, []); 
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(editCategory) {
+      const response = await axios.put(`http://localhost:3000/api/category/${editCategory}`, {categoryName, categoryDescription}, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
+        },
+      }
+    );
+    if (response.data.success) {
+      setEditCategory(null);
+      alert("Category Updated successfully");
+      fetchCategories(); // Refresh the category list after adding a new category
+    }else {
+      console.error("Error editing the Category:", data);
+      alert("Error editing the Category. Please try again")
+    }
+    } else {
       const response = await axios.post("http://localhost:3000/api/category/add", {categoryName, categoryDescription}, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
@@ -43,12 +62,28 @@ const Categories = () => {
       setCategoryName("");
       setCategoryDescription("");
       alert("Category added successfully");
+      fetchCategories(); // Refresh the category list after adding a new category
     }else {
       console.error("Error adding the Category:", data);
       alert("Error adding the Category. Please try again")
     }
+  }
+
+  };
 
 
+
+
+  const handleEdit = async (category) => {
+    setEditCategory(category._id);
+    setCategoryName(category.categoryName);
+    setCategoryDescription(category.categoryDescription);
+  };
+
+  const handleCancel = async () => {
+    setEditCategory(null);
+    setCategoryName("");
+    setCategoryDescription("");
   }
 
   if (loading) {
@@ -60,16 +95,25 @@ const Categories = () => {
       <h1 className="categories-title">Category Management</h1>
       <div className="categories-content">
         <div className="add-category-section">
-          <h2 className="add-category-title">Add Category</h2>
+          <h2 className="add-category-title">{editCategory ? "Edit Category" : "Add Category"}</h2>
           <form className="add-category-form" onSubmit={handleSubmit}>
             <div className="form-group">
-              <input type="text" className="input" placeholder="Category Name" onChange={(e) => setCategoryName(e.target.value)} />
+              <input type="text" className="input" value={categoryName} placeholder="Category Name" onChange={(e) => setCategoryName(e.target.value)} />
 
             </div>
             <div className="form-group">
-              <input type="text" className="input" placeholder="Category Description" name="Category Description" onChange={(e) => setCategoryDescription(e.target.value)}   />
+              <input type="text" className="input" value={categoryDescription} placeholder="Category Description" name="Category Description" onChange={(e) => setCategoryDescription(e.target.value)}   />
             </div>
-            <button type="submit" className="add-category-btn">Add Category</button>
+          <div>
+            <button type="submit" className="add-category-btn">{editCategory ? "Save Changes" : "Add Category"}</button>
+            {
+              editCategory && (
+                <button type='button' className='w-full mt-2 rounded-md bg-red-500 text-white p-3 cursor-pointer hover:bg-red-600' onClick={handleCancel}>
+                  Cancel
+                </button>
+              )
+            }
+          </div>   
           </form>
         </div>
         <div className="category-list-section">
@@ -77,18 +121,18 @@ const Categories = () => {
             <table className="category-table">
               <thead>
                 <tr className="category-table-header-row">
-                  <th className="category-table-header">Category Name</th>
-                  <th className="category-table-header">Category Description</th>
+                  <th className="category-table-header">ID</th>
+                  <th className="category-table-header">Name</th>
                   <th className="category-table-header">Action</th>
                 </tr>
               </thead>
               <tbody>
-                {categories.map((category) => (
-                  <tr key={category._id} className="category-table-row">
+                {categories.map((category, index) => (
+                  <tr key={index} className="category-table-row">
+                    <td className="category-table-cell">{index + 1}</td>
                     <td className="category-table-cell">{category.categoryName}</td>
-                    <td className="category-table-cell">{category.categoryDescription}</td>
                     <td className="category-table-cell">
-                      <button className="category-edit-btn">Edit</button>
+                      <button className="category-edit-btn" onClick={() => handleEdit(category)}>Edit</button>
                       <button className="category-delete-btn">Delete</button>
                     </td>
                   </tr>
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addCategory, getCategories } from '../controllers/categoryController.js';
+import { addCategory, getCategories, updateCategory } from '../controllers/categoryController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 
@@ -7,5 +7,5 @@ const router = express.Router();
 
 router.post('/add', authMiddleware, addCategory);
 router.get('/', authMiddleware, getCategories);
-
+router.put('/:id', authMiddleware, updateCategory); 
 export default router;
